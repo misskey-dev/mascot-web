dd764bc13a5d982de4a4fbb378e073446fb15791
syuilo <Syuilotan@yahoo.co.jp>
2021-09-04 13:15:07 +0900

    improve

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,7 @@
 import { markRaw, onMounted, ref } from 'vue';
 import { load } from './live2d/index';
 
-let live2d = null;
+let live2d: any = null;
 const canvas = ref<HTMLCanvasElement>();
 
 const touched = (ev: MouseEvent) => {
@@ -10,10 +10,14 @@ const touched = (ev: MouseEvent) => {
 };
 
 onMounted(() => {
-	console.log(canvas.value);
+	const url = new URL(window.location.href);
+
 	load(canvas.value!, {
-		scale: 1.6,
-		y: 1.1
+		scale: parseInt(url.searchParams.get('scale') || '1', 10),
+		x: parseInt(url.searchParams.get('x') || '0', 10),
+		y: parseInt(url.searchParams.get('y') || '1', 10),
+		eyeX: url.searchParams.has('eyeX') ? parseInt(url.searchParams.get('eyeX')!, 10) : undefined,
+		eyeY: url.searchParams.has('eyeY') ? parseInt(url.searchParams.get('eyeY')!, 10) : undefined,
 	})
 	.then(_live2d => {
 		live2d = markRaw(_live2d);
--- src/live2d/index.ts
+++ src/live2d/index.ts
@@ -29,7 +29,7 @@ const MODEL_FILES = {
 
 let isCoreSdkLoaded = false;
 
-export function load(canvas: HTMLCanvasElement, options: { x?: number; y?: number; scale?: number; }) {
+export function load(canvas: HTMLCanvasElement, options: { x?: number; y?: number; scale?: number; eyeX?: number; eyeY?: number; }) {
 	return new Promise((res, rej) => {
 		const existedCoreSdkScript = document.head.querySelector(`script[src="${CUBISM_CORE_URL}"]`);
 		if (existedCoreSdkScript) {
@@ -58,7 +58,7 @@ export function load(canvas: HTMLCanvasElement, options: { x?: number; y?: numbe
 	});
 }
 
-async function main(canvas: HTMLCanvasElement, options: { x: number; y: number; scale: number; }) {
+async function main(canvas: HTMLCanvasElement, options: { x: number; y: number; scale: number; eyeX?: number; eyeY?: number; }) {
 	try {
 		const [model, moc3, physics, textures, expressions, motions] = await Promise.all([
 			fetch(MODEL_FILES.model3).then(res => res.arrayBuffer()),
@@ -86,28 +86,41 @@ async function main(canvas: HTMLCanvasElement, options: { x: number; y: number; 
 			...options
 		});
 		const point = new FacePoint();
-		const _handleOnMouseMove = (e: MouseEvent) => {
-			const x = e.clientX;
-			const y = e.clientY;
+		const _handleOnMouseMove = (e: {
+			x: number;
+			y: number;
+		}) => {
+			const cursorX = e.x;
+			const cursorY = e.y;
 			const rect = canvas.getBoundingClientRect();
-			const cx = rect.left + rect.width / 2;
-			const cy = rect.top + rect.height / 2;
-			const distance = getDistance(x, y, cx, cy);
-			const dx = cx - x;
-			const dy = cy - y;
-			const angle = getAngle(x, y, cx, cy);
+
+			// モデルの目の位置
+			// 指定されていない場合、とりあえず目は画像の真ん中あたりにあると仮定する
+			// TODO: 自動でモデルの目の位置を取得し、指定の必要を無くす(出来るか不明)
+			const eyeX = options.eyeX ? options.eyeX : rect.left + (rect.width / 2);
+			const eyeY = options.eyeY ? options.eyeY : rect.top + (rect.height / 2);
+
+			const angle = getAngle(cursorX, cursorY, eyeX, eyeY);
 			const r = Math.cos(angle) * Math.sin(angle) * 180 / Math.PI;
+			const distance = getDistance(cursorX, cursorY, eyeX, eyeY);
+			const dx = eyeX - cursorX;
+			const dy = eyeY - cursorY;
 			Object.assign(point, {
 				angleX: -dx / 10,
 				angleY: dy / 10,
-				angleZ: r * (distance / cx),
-				angleEyeX: -dx / cx,
-				angleEyeY: dy / cy,
+				angleZ: r * (distance / eyeX),
+				angleEyeX: -dx / eyeX,
+				angleEyeY: dy / eyeY,
 			});
 			renderer.updatePoint(point);
 		};
 
-		document.body.addEventListener('mousemove', _handleOnMouseMove, false);
+		document.body.addEventListener('mousemove', ev => {
+			_handleOnMouseMove({
+				x: ev.clientX,
+				y: ev.clientY
+			});
+		}, false);
 
 		window.addEventListener('message', message => {
 			const { type, body } = message.data;
