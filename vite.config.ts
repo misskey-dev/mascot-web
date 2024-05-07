37c0f0b7cd0ac1eb1a3582c517b2e5ff47dadbf7
かっこかり <67428053+kakkokari-gtyih@users.noreply.github.com>
2024-05-07 18:33:30 +0900

    enhance: Live2D読み込み完了時にpostMessageを発火 (#3)
    * update deps
    
    * enhance: Live2D読み込み完了時にpostMessageを発火
    
    * fix

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,10 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -14,5 +19,5 @@ export default defineConfig({
 	build: {
 		assetsDir: 'built'
 	},
-  plugins: [vue()]
+	plugins: [vue()]
 })
